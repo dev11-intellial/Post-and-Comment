1a06e92264c487a5e54300445e6821f7658cdab7
dev11-intellial <50742644+dev11-intellial@users.noreply.github.com>
2025-05-01 14:31:23 +0530

    changes of test file

--- post_app/tests.py
+++ post_app/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User,Group,Permission
 # Create your tests here.
+
+
+
+user = User.objects.get(username='admin123')
+groups = Group.objects.all()
+print("groups", groups)
+print(user.groups)
